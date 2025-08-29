8f5fcf5c9105de5045f25a9a9918b194acd43972
PriyanshCG <priyansh.patel.cg@gmail.com>
2025-08-29 11:22:31 +0530

    new

---
+++ coding-gita/C-language/ifwhile.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+ //   for(int i=10;i<=50;i++){
+//        if(i%2==1){
+ //           printf("%d\n",i);
+//        }
+ //   }
+ 
+ 
+ int i=10;
+ while(i<=50){
+     if(i%2==1)
+     printf("%d\n",i);
+ }
+    i++;
+    return 0;
+}
+
