dcce26bae998e66f0e321aaac1f0771bd8098d58
PriyanshCG <priyansh.patel.cg@gmail.com>
2025-10-08 11:09:39 +0530

    javascript practise questions assignment2

---
+++ coding-gita/js/javabasic.js
@@ -0,0 +1,23 @@
+// var arr =[1,2,3,4,5];
+// console.log(arr);
+// console.log(arr[0]);
+// console.log(arr[3]);
+// var sum = 0;
+// for(var i=0;i<5;i++){
+//     sum=sum+arr[i];
+// }
+// console.log("sum is",sum);
+
+var arr =[1,2,3,4,5];
+console.log(arr);
+console.log(arr[0]);
+console.log(arr[3]);
+var sum = 0;
+for(var i=0;i<5;i++){
+    if(arr[i]%2==0){
+        sum=sum+arr[i];
+    }
+   
+}
+ console.log("sum is",sum);
+ 
