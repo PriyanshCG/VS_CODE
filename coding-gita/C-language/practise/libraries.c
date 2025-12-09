ba66e7deaaf67ec84960b688f86b6219a9a61b9e
PriyanshCG <priyansh.patel.cg@gmail.com>
2025-12-09 14:47:18 +0530

    changes of files

---
+++ coding-gita/C-language/practise/libraries.c
@@ -0,0 +1,17 @@
+// #include<stdio.h>
+// #include<math.h>
+
+// int main(){
+//     double x=25.0;
+//     printf("sqrt(%.2f) = %.2f\n",x,sqrt(x));
+//     return 0;
+// }
+
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    printf("2^3 = %.2f\n",pow(2,3));
+
+    return 0;
+}
