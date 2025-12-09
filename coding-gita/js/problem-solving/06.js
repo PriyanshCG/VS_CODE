63e10f5a1db5595bd00933a27b8ba38628ae9292
PriyanshCG <priyansh.patel.cg@gmail.com>
2025-12-09 16:09:24 +0530

    no 53 added to 06.js

--- coding-gita/js/problem-solving/06.js
+++ coding-gita/js/problem-solving/06.js
@@ -24,3 +24,17 @@
 // }
 
 // console.log(sum); 
+
+
+
+//53
+// let arr = [3, 7, 2, 9];
+// let max = arr[0];
+
+// for (let i = 1; i < arr.length; i++) {
+//   if (arr[i] > max) {
+//     max = arr[i];
+//   }
+// }
+
+// console.log(max);   
