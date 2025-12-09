cc82f2460c963f50295185df9fec485fae915200
PriyanshCG <priyansh.patel.cg@gmail.com>
2025-12-09 16:04:29 +0530

    files added

---
+++ coding-gita/js/problem-solving/06.js
@@ -0,0 +1,26 @@
+//q51
+// let arr = [1, 2, 3];
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+//other way
+// let arr = [1, 2, 3];
+// console.log(arr.join(" "));
+
+
+//other way
+// let arr = [1, 2, 3];
+// console.log(...arr);
+
+
+
+//52
+// let arr = [1, 2, 3];
+// let sum = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+//   sum += arr[i];
+// }
+
+// console.log(sum); 
