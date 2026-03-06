8111f1fadcf34bc9168abeb6f4e980577eafa5a6
PriyanshCG <priyansh.patel.cg@gmail.com>
2026-03-06 10:03:16 +0530

    content changed in file arithmeticOperators.cpp

--- coding-gita/cpp/basic/arithmeticOperators.cpp
+++ coding-gita/cpp/basic/arithmeticOperators.cpp
@@ -1,9 +1,16 @@
 #include<iostream>
 using namespace std;
 int main(){
-  int x = 7, y = 2;
-  cout<<x+y<<endl; //9
-  cout<<x-y<<endl; // 5
-  cout<<x*y<<endl; // 14
-  cout<<x/y<<endl; // 3.5
+//   int x = 7, y = 2;
+//   cout<<x+y<<endl; //9
+//   cout<<x-y<<endl; // 5
+//   cout<<x*y<<endl; // 14
+//   cout<<x/y<<endl; // 3.5
+
+  float x = 7.2;
+  float y = 2.5;
+  cout<<x+y<<endl; 
+  cout<<x-y<<endl; 
+  cout<<x*y<<endl; 
+  cout<<x/y<<endl; 
 }
